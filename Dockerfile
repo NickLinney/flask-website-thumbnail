8ef3f3f695e31d8d08407fa01aacf48dae596ae8
Nick Linney <nrlinney@live.com>
2023-02-20 08:33:28 +0000

    Refactor for geckodriver and Ubuntu Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,46 @@
+FROM ubuntu:20.04
+
+# Update apt-get
+RUN apt-get update
+
+# Install Python 3 and pip
+RUN apt-get install -y python3 python3-pip
+
+# Install Selenium dependencies
+RUN apt-get install -y \
+  firefox \
+  chromium-browser \
+  unzip \
+  wget \
+  libglib2.0-0 \
+  libnss3 \
+  libgconf-2-4 \
+  libfontconfig1 \
+  xvfb
+
+# Install geckodriver
+RUN wget https://github.com/mozilla/geckodriver/releases/download/v0.30.0/geckodriver-v0.30.0-linux64.tar.gz && \
+    tar -xzf geckodriver-v0.30.0-linux64.tar.gz && \
+    mv geckodriver /usr/local/bin/ && \
+    rm geckodriver-v0.30.0-linux64.tar.gz
+
+# Install chromedriver
+RUN wget https://chromedriver.storage.googleapis.com/99.0.4844.51/chromedriver_linux64.zip && \
+    unzip chromedriver_linux64.zip && \
+    mv chromedriver /usr/local/bin/ && \
+    rm chromedriver_linux64.zip
+
+# Set display environment variable
+ENV DISPLAY=:99
+
+# Copy the application files
+COPY . /app
+
+# Set the working directory
+WORKDIR /app
+
+# Install the application dependencies
+RUN pip3 install -r requirements.txt
+
+# Run the application
+CMD [ "python3", "app.py" ]
--- app.py
+++ app.py
@@ -1,50 +1,23 @@
 from flask import Flask, request, send_file
 from selenium import webdriver
-import os
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from io import BytesIO
 
 app = Flask(__name__)
 
-# Set the path to the chromedriver executable
-chromedriver_path = "/path/to/chromedriver"
-
-# Define the function to generate a thumbnail
-def generate_thumbnail(url):
-    # Set the options for the headless Chrome browser
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--window-size=1200x600')
-
-    # Set the path to the chromedriver executable
-    chromedriver_path = "/path/to/chromedriver"
-    browser = webdriver.Chrome(chromedriver_path, options=options)
-
-    # Open the webpage and take a screenshot
-    browser.get(url)
-    browser.set_window_size(1200, 600) # This is larger than 300x300 so we can take a high resolution image
-    screenshot_path = os.path.join(os.getcwd(), "screenshot.png")
-    browser.save_screenshot(screenshot_path)
-
-    # Crop and resize the image to a thumbnail
-    from PIL import Image
-    image = Image.open(screenshot_path)
-    image = image.crop((0, 0, 1200, 600)) # Crop to the same size as the window
-    image.thumbnail((300, 300)) # Resize to 300x300 pixels
-    thumbnail_path = os.path.join(os.getcwd(), "thumbnail.png")
-    image.save(thumbnail_path)
-
-    # Clean up the browser and return the thumbnail
-    browser.quit()
-    return thumbnail_path
-
-# Define the Flask route to handle thumbnail requests
-@app.route("/thumbnail", methods=["GET"])
-def thumbnail():
+@app.route('/thumbnail')
+def get_thumbnail():
     url = request.args.get('url')
-    thumbnail_path = generate_thumbnail(url)
-    return send_file(thumbnail_path, mimetype='image/png')
+    firefox_options = FirefoxOptions()
+    firefox_options.add_argument('-headless')
+    firefox_service = FirefoxService(executable_path='/usr/local/bin/geckodriver')
+    browser = Firefox(service=firefox_service, options=firefox_options)
+    browser.get(url)
+    screenshot = browser.get_screenshot_as_png()
+    browser.quit()
+    return Response(screenshot, mimetype='image/png')
 
-# Run the app
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0')
